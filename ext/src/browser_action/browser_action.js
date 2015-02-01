dddcc6a3a1ddf5a0022be74c5e5a56d3cd098985
torpeyp <torpey.patj@gmail.com>
2015-01-31 19:03:46 -0500

    names added

--- ext/src/browser_action/browser_action.js
+++ ext/src/browser_action/browser_action.js
@@ -4,8 +4,13 @@ var f = new Firebase('https://inifinitidrive.firebaseio.com/');
 var users = f.child("users");
 var requests = f.child("requests");
 
-$('#help-btn').on('click', function() {
-    requests.push({ username: 'Bob-Knows-Nothing', url: 'https://github.com/bs', done: false });
+$('#help-btn').on('click', function(event) {
+    event.preventDefault();
+    var name = $("#name").val();
+    var link = $("#link").val();
+    requests.push({ username: name, url: link, done: false });
+    $("#name").val() = "";
+    $("#link").val() = "";
 });
 
 $('#requests').on('click', 'p', function(e) {
