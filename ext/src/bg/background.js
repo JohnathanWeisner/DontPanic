c936e90c736c85a8fd75903dea0e8bf5e4246d08
Kenn Breece <kjb085@gmail.com>
2015-04-15 04:00:17 -0400

    Fix badge icon never decreasing bug

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -4,13 +4,22 @@ var f = new Firebase('https://inifinitidrive.firebaseio.com/');
 var requests = f.child("requests")
 
 var output = 0
+var new_output = 0
 
 requests.limit(100).on('child_added', function(req) {
 	request = req.val()
-	console.log(request)
     if (!(request.done)){
     	output += 1
 		  chrome.notifications.create('new_request', {type: 'basic', title: request.username + ' needs help!', message: "Now's your time coach!", iconUrl: '../../icons/dont_panic.png'}, function(){})
-    };
+    }
 	chrome.browserAction.setBadgeText({text: output.toString()})
+});
+
+requests.limit(100).on('child_changed', function(req) {
+    request = req.val()
+    console.log(request)
+    if (request.done){
+        output -= 1
+    }
+    chrome.browserAction.setBadgeText({text: output.toString()})
 });
