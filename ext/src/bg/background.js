ed7d990c7b27668a1b23f41b70fb566a63d521f1
Kenn Breece <kjb085@gmail.com>
2015-04-16 21:18:06 -0400

    Fix notification bug

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -4,12 +4,12 @@ var f = new Firebase('https://inifinitidrive.firebaseio.com/');
 var requests = f.child("requests")
 
 var output = 0
-var new_output = 0
 
 requests.limit(100).on('child_added', function(req) {
 	request = req.val()
     if (!(request.done)){
     	output += 1
+      chrome.notifications.clear('new_request', function(){})
 		  chrome.notifications.create('new_request', {type: 'basic', title: request.username + ' needs help!', message: "Now's your time coach!", iconUrl: '../../icons/dont_panic.png'}, function(){})
     }
 	chrome.browserAction.setBadgeText({text: output.toString()})
